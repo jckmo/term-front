1d329e98fc4aadfdcb55b5755463f531f2da78ee
Jack <jacksonmorgan@hey.com>
2021-09-19 13:44:30 -0400

    catch up; set up index and clear out junk

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+
+import appReducer from './reducers/appReducer.js'
+
+// const backend = 
+
+const store = createStore(appReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <Route exact path='/' component='App'/>
+    </Router>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
